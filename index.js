5174a7c762609ea22ac3e0eb35050d6610b0aa8e
Matt Tilley <mtilley255@gmail.com>
2017-09-09 09:23:02 -0500

    Removed practice code / clean slate.

--- index.js
+++
@@ -1,9 +0,0 @@
-function init()
-{
-	document.getElementById("panel").innerHTML = "This is from external JS file.";
-	
-	window.alert("KLJASDLKASJDLKSAJDLKJSADLKJSADLKJSADLKJ");
-	
-}
-
-window.onload = init();
