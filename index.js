e085713d4d0700d3f524b5ae62e435a99738cc20
Matt Tilley <mtilley255@gmail.com>
2017-09-09 10:16:20 -0500

    Set-up for next phase of P21 practice.

---
+++ index.js
@@ -0,0 +1,10 @@
+function init()
+{
+	var str = "Text content";
+	var num = 100;
+	var bln = true;
+	var fcn = init;
+	var obj = document.getElementById("panel");
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
