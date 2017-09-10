180cd5e876fcef660b3f9db1e91145dcccda92a1
Matt Tilley <mtilley255@gmail.com>
2017-09-10 13:26:23 -0500

    Example P 42 / part #1

---
+++ index.js
@@ -0,0 +1,13 @@
+function buttonReport(buttonId, buttonName, buttonValue)
+{
+	var userMessage1 = "Button id: " + buttonId + "\n";
+	
+	var userMessage2 = "Button name: " + buttonName + "\n";
+	
+	var userMessage3 = "Button value " + buttonValue;
+	
+	alert(userMessage1 + " " + userMessage2 + " " + userMessage3);
+}
+
+buttonReport(12, "THIS ONE", "FLURP");
+
