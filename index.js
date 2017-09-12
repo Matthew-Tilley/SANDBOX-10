0a393533d2830bd4389b8c36f8c045c9bbdae1cb
Matt Tilley <mtilley255@gmail.com>
2017-09-12 08:49:29 -0500

    Output example code to element / made it more readable on screen.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mydate = new Date;
 document.getElementById("F1").innerHTML = mydate.getDay();
 var numdate = mydate.setDate(15);
-document.write("After amending date to the 15th, the day number is: " + mydate.getDay());
+
+document.getElementById("F1").innerHTML = ("After amending date to the 15th, the day number is: " + mydate.getDay());
