8ae32e6025389210b89c68ae9e218f938599c470
Matt Tilley <mtilley255@gmail.com>
2017-09-12 08:56:22 -0500

    Clean-slated for next example task.

--- index.js
+++
@@ -1,4 +0,0 @@
-var mydate = new Date;
-console.log("Object created on day number " + mydate.getDay() );
-mydate.setDate(15);
-console.log("After amending date to 15th, the day number is " + mydate.getDay());
