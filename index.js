c31ffb8360f9002029e6fc04b7ee93a7f5bb55f7
Matt Tilley <mtilley255@gmail.com>
2017-09-12 09:04:16 -0500

    Found error / corrected - invalid syntax entry.
    Line #5 - forgot the () on end of getDate() method.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var mydate = new Date;
 
 console.log( "Created: " + mydate.toDateString() + " " + mydate.toTimeString() );
 
-mydate.setDate( mydate.getDate + 33 );
+mydate.setDate( mydate.getDate() + 33 );
 
 console.log( "After adding 33 days: " + mydate.toDateString() + " " + mydate.toTimeString() );
