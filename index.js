81a4ae730d5275155cd8536be236ba42017193e2
Matt Tilley <mtilley255@gmail.com>
2017-09-12 08:56:00 -0500

    Repeated example on P61 / output to console instead for readability.
    Still getting different results for some reason; not quite sure what is going on.  Moving onward, so I don't get bogged-down with confusion & frustration; can always revisit the issue later.

---
+++ index.js
@@ -0,0 +1,4 @@
+var mydate = new Date;
+console.log("Object created on day number " + mydate.getDay() );
+mydate.setDate(15);
+console.log("After amending date to 15th, the day number is " + mydate.getDay());
