d3e47886fca855de9e4f2f1693a7525ef66c7160
Matt Tilley <mtilley255@gmail.com>
2017-09-12 08:50:47 -0500

    Clean-slated JS file / potential issue with previous code.
    Reading further down the page, I am getting different output for some reason.

--- index.js
+++
@@ -1,5 +0,0 @@
-var mydate = new Date;
-document.getElementById("F1").innerHTML = mydate.getDay();
-var numdate = mydate.setDate(15);
-
-document.getElementById("F1").innerHTML = ("After amending date to the 15th, the day number is: " + mydate.getDay());
