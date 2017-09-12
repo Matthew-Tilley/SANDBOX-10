acecdf8fede5a622535c3970333d0c0716b5c81e
Matt Tilley <mtilley255@gmail.com>
2017-09-12 09:05:58 -0500

    Clean-slated HTML & JS files for next practice task.

--- index.js
+++
@@ -1,7 +0,0 @@
-var mydate = new Date;
-
-console.log( "Created: " + mydate.toDateString() + " " + mydate.toTimeString() );
-
-mydate.setDate( mydate.getDate() + 33 );
-
-console.log( "After adding 33 days: " + mydate.toDateString() + " " + mydate.toTimeString() );
