3bf4b05e1f1f2d6a54eab624f3b8b5baa53d3a88
Matt Tilley <mtilley255@gmail.com>
2017-09-12 09:02:58 -0500

    Not outputting correctly for some reason.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var mydate = new Date;
 
-console.log( "created: " + mydate.toDateString() + " " + mydate.toTimeString() );
+console.log( "Created: " + mydate.toDateString() + " " + mydate.toTimeString() );
+
+mydate.setDate( mydate.getDate + 33 );
+
+console.log( "After adding 33 days: " + mydate.toDateString() + " " + mydate.toTimeString() );
