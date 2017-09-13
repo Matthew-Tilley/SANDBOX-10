9e01323fa195084369aa883f27a00fcc005e80f7
Matt Tilley <mtilley255@gmail.com>
2017-09-12 20:09:36 -0500

    P63 - Same thing, with exception of using Math.max()

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var M = 27;
 var C = 31;
 var S = 19;
 
-console.log("The youngest person is: " + Math.min(D, M, C, S) + " years old.");
+console.log("The oldest person is: " + Math.max(D, M, C, S) + " years old.");
