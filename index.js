6b4e50bdc0450b8748dd4a5d87f438acd7ef675e
Matt Tilley <mtilley255@gmail.com>
2017-09-13 09:06:11 -0500

    P 68 - Exercise #2 / created function calculate() & refactored previous
    (Placed previous code associated with var volume into the scope of a function, instead of hanging out there globally.)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var height = prompt("Enter height of cylinder, in meters: ");
 var radius = prompt("Enter the radius of cylinder, in meters: ");
 
-var volume = Math.ceil(Math.PI * Math.pow(radius, 2) * height);
+function calculate()
+{
+	var volume = Math.ceil(Math.PI * Math.pow(radius, 2) * height);
+	console.log(volume);
+}
 
-console.log(volume);
+
