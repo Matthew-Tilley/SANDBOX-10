25b0908d761b65fd5801788617dca3ee66852331
Matt Tilley <mtilley255@gmail.com>
2017-09-13 09:31:59 -0500

    P68 - Exercise #2 / making sausage (code not working / debugging)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+document.getElementById("I2").innerHTML = "LKALKJASDLKJ";
+console.log("LKJLKJ");
+
+/*
 var height = prompt("Enter height of cylinder, in meters: ");
 var radius = prompt("Enter the radius of cylinder, in meters: ");
 
 function calculate()
 {
 	var volume = Math.ceil(Math.PI * Math.pow(radius, 2) * height);
-	console.log(volume);
+	document.getElementById("T1").innerHTML = volume.toString();
 }
 
 
+
+*/
