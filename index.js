8647ce7a40807d3fee3d5fead5b8a5d0b82810fd
Matt Tilley <mtilley255@gmail.com>
2017-09-14 09:16:31 -0500

    Final save / work from previous session.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-document.getElementById("I2").innerHTML = "LKALKJASDLKJ";
-console.log("LKJLKJ");
 
-/*
 var height = prompt("Enter height of cylinder, in meters: ");
 var radius = prompt("Enter the radius of cylinder, in meters: ");
 
 function calculate()
 {
 	var volume = Math.ceil(Math.PI * Math.pow(radius, 2) * height);
-	document.getElementById("T1").innerHTML = volume.toString();
+	document.getElementById("I1").innerHTML = volume.toString();
 }
 
 
 
-*/
