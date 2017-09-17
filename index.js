0c90b7e36077cd35108d2fae1b9c2f40b3fefbdb
Matt Tilley <mtilley255@gmail.com>
2017-09-17 11:00:07 -0500

    P75 - Create something (out of my own mind) that does...something.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-var name = prompt("Please enter your first name: ");
+var first_name = prompt("Please enter your first name: ");
+
+var first_initial = console.log("Your first initial is: " + first_name.substr(0, 1));
