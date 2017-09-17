bd0372d9505ff6b0edef6c380f0bb43aa998121a
Matt Tilley <mtilley255@gmail.com>
2017-09-17 11:03:11 -0500

    P75 - Added button functionality...just for fun.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var first_name = prompt("Please enter your first name: ");
+function extractor()
+{
+	var first_name = prompt("Please enter your first name: ");	
 
-var first_initial = console.log("Your first initial is: " + first_name.substr(0, 1));
+		var first_initial = console.log("Your first initial is: " + first_name.substr(0, 1));
+}
