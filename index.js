c936b2f1cc2153e37e6efceb0de205c0dad6ceb3
Matt Tilley <mtilley255@gmail.com>
2017-09-18 09:00:59 -0500

    P76 - created function with if / else statements...not working.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var input = prompt('Enter some text with the string of "fake" at some point.');
+var input = prompt('Enter some text with the string of "fake" at some point, in all caps!');
 
 function detectSpam(input)
 {
+	input = input.toLowerCase();
 	console.log(input);
+	
+	if (input.indexOf("fake") > 0)
+	{
+		console.log("The Orange Terror declares this fake news!");
+	}
+	
+	else
+	{
+		console.log("The Orange Terror approves.");
+	}
 	
 }
 
