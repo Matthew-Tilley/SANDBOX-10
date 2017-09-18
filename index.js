274af56090e8229b82eb73d73df37d95abaff61b
Matt Tilley <mtilley255@gmail.com>
2017-09-18 08:30:54 -0500

    P75 - created if / else statement & output bool value to console.

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ function confirmz()
 {
 	var check = confirm("Are you absolutely,\npositively sure\nyou want to continue???");
 	
+	if (check == true)
+	{
+		console.log("They want to continue.");
+	}
 	
+	else
+	{
+		console.log("User is not continuing.");
+	}
 }
