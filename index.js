f772b65c148d441a98d165007bf405fd9580b421
Matt Tilley <mtilley255@gmail.com>
2017-09-20 18:59:52 -0500

    P90 - second example (expanded).

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+var message = " ";
+var temperature = 60;
 
+if (temperature < 60)
+{
+	message = "Turn on the heat.";
+}
 
+else 
+{
+	message = "Turn on the air conditioning."
+}
+
+console.log(message);
