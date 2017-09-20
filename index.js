d64469807444ba41adaf71ba51251b2248d99a8a
Matt Tilley <mtilley255@gmail.com>
2017-09-20 14:42:59 -0500

    P82 - brought back elements of previous code for next example.
    No need to reinvent the wheel, in this particular case...

---
+++ index.js
@@ -0,0 +1,11 @@
+var myArray = [];
+
+myArray[1] = "Monday";
+myArray[3] = "Wednesday";
+myArray[5] = "Friday";
+myArray[0] = "Sunday";
+myArray[2] = "Tuesday";
+myArray[4] = "Thursday";
+myArray[6] = "Saturday";
+
+
