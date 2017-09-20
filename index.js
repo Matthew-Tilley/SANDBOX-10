ea483c1076fed41f1dbf48f8c41ac89c2c656d2c
Matt Tilley <mtilley255@gmail.com>
2017-09-20 09:23:32 -0500

    P81 - created new variable days_of_week.

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-var a = [1,2,3,4,5,6,7,8,9];
+var days_of_week = [];
 
-for (i=0; i<a.length; i++)
-{
-	if ( a[i] %2 == 0)
-	{
-		console.log("The number " + a[i] + " is even.");
-	}
-	
-	else
-	{
-		console.log("The number " + a[i] + " is odd.");
-	}
-}
