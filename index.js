7d756a9bab678703186f82d30d37655d0ff14bcb
Matt Tilley <mtilley255@gmail.com>
2017-09-20 18:55:08 -0500

    Clean-slated HTML & JS files for next practice task.

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-function activate()
-{
 
-	var cointoss = Math.round(Math.random());
-	
-	if (cointoss == 1)
-	{
-		console.log("HEADS");
-	}
-	
-	else
-	{
-		console.log("TAILS");
-	}
-	
-}
-
-function newGame()
-{
-	location.reload();
-}
 
