b73cfcc3d8e3121778de59db61f579e70e7d4c24
Matt Tilley <mtilley255@gmail.com>
2017-09-20 14:52:47 -0500

    P82 - tinkering / experimenting with the concat() method.

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ myArray_1.push("Monday", "Tuesday", "Wednesday", "Thursday");
 
 myArray_2.push("Friday", "Saturday", "Sunday");
 
-console.log(myArray_1, myArray_2);
-
-console.log(myArray_1.concat(myArray_2));
+console.log(myArray_1[2].concat(myArray_2[2]));
