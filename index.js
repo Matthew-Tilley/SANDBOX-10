70cf9c6d7c693a4e552068d9574a696468179a98
Matt Tilley <mtilley255@gmail.com>
2017-09-20 15:00:33 -0500

    P83 - join() method

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ myArray_1.push("Monday", "Tuesday", "Wednesday", "Thursday");
 
 myArray_2.push("Friday", "Saturday", "Sunday");
 
-var weekend = myArray_2[1].concat(myArray_2[2]);
-
-console.log(myArray_1.length);
+console.log(myArray_1.join("%%%"));
