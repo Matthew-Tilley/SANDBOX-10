f2aa59fc77a0a94df2247e5c60128673f297ef76
Matt Tilley <mtilley255@gmail.com>
2017-09-20 18:26:30 -0500

    P85 - completed listing 6.1 per example.

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ var newSentence = " ";
 document.getElementById("div1").innerHTML = "<p>" + sentence + "</p>";
 
 var words = sentence.split(" ");
+
+console.log(words);
+
+var message = words.splice(3,2, "powerful");
+
+alert("Removed words: " + message);
+
+document.getElementById("div2").innerHTML = "<p>" + words.join(" ") + "</p>";
