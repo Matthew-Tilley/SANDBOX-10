2cd269ae964b02942ffc2804d9ad8ba5c95c8c86
Matt Tilley <mtilley255@gmail.com>
2017-09-20 18:20:09 -0500

    P85 - added CSS styling for readability.

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var sentence = "Javascript is a really cool language!";
 var newSentence = " ";
 
 document.getElementById("div1").innerHTML = "<p>" + sentence + "</p>";
+
+var words = sentence.split(" ");
