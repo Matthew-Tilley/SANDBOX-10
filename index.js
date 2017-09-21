44f21892d7eb5091baee4748d75204d0fc97d037
Matt Tilley <mtilley255@gmail.com>
2017-09-21 11:16:30 -0500

    P95 - first attempt at switch statement / not going well.

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var temp = prompt("Enter the current temperature.");
 
-if ( temp < 64 )
+switch(temp)
 {
-	console.log("Turn on the heater.");
-}
-
-else if ( temp > 64 && temp < 73 )
-{
-	console.log("All is well.");
-}
-
-else
-{
-	console.log("Turn on the air conditioning!");
+	case "< 64" :
+	console.log("TURN ON THE HEAT.");
+	break;
 }
