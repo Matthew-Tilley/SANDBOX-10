ba9202bd7e5b41ad9cddad6b1cb6248c1fbd1759
Matt Tilley <mtilley255@gmail.com>
2017-09-23 15:56:15 -0500

    P95 -significangly expanded functionality / incorporated switch statemt

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
-var color = "green";
-
-switch(color)
+function randomizer()
 {
-	case color = "red" :
-	message = console.log("Stop!");
-	break;
+	var number = Math.ceil(Math.random() * 3);
+	return color_assignment(number);
+}
+
+
+function color_assignment(number)
+{
+	var color;
 	
-	case color = "yellow" :
-	message = console.log("Proceed with caution!");
-	break;
-		
-	case color = "green" :
-	message = console.log("You may proceed.");
-	break;
+	if (number == 1)
+	{
+		color = "green";
+	}
+	
+	if (number == 2)
+	{
+		color = "yellow";
+	}
+	
+	if (number == 3)
+	{
+		color = "red";
+	}
+	
+	return output(color);
 	
 }
+
+
+function output(color)
+{
+	switch(color)
+			{
+		case color = "red" :
+			message = console.log("Stop!");
+			break;
+			
+		case color = "yellow" :
+			message = console.log("Proceed with caution!");
+			break;
+			
+		case color = "green" :
+			message = console.log("You may proceed.");
+			break;
+			
+	}
+}
+
+
 
 
