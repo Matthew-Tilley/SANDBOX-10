4d4c7ba6302ac00b1e9ab66d9eea833e6dd1aea6
Matt Tilley <mtilley255@gmail.com>
2017-09-23 16:09:54 -0500

    P96 - expanded on example / not working properly.

---
+++ index.js
@@ -0,0 +1,25 @@
+var temperature = prompt("Enter the temperature:");
+
+var access_code = prompt("Enter your access code:");
+
+var user_access_code = "123";
+
+if (temperature < 64 && access_code == user_access_code)
+{
+	console.log("You may turn on the heat.");
+}
+
+else if (temperature > 64 || temperature < 75 && access_code == user_access_code)
+{
+	console.log("No action is required");
+}
+
+else if (temperature > 75 && access_code == user_access_code)
+{
+	console.log("You may turn on the air conditioning.");
+}
+
+else if (access_code != user_access_code)
+{
+	console.log("ACCESS DENIED - ENTER CREDENTIALS AGAIN.");
+}
