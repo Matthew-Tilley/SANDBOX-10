edc084d3650623adfd93e6eb6131a733e831cddc
Matt Tilley <mtilley255@gmail.com>
2017-09-23 18:46:51 -0500

    Think I'm generally on the right track...need to refactor.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function possible()
 {
 	var number = Math.ceil(Math.random() * 69 );
-	return number;
+	return checker(number);
 }
 
- console.log(possible());
+
+function checker(number)
+{
+	var white_balls = [];
+	
+	for ( i in white_balls )
+	{
+		if ( number != white_balls[i])
+		{
+			white_balls.push();
+		}
+	}
+	
+	return console.log(white_balls);
+	
+}
+
 
 
 
