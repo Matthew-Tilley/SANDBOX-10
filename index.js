7a0114f0ea5fb053e936337aff303153d68c6b90
Matt Tilley <mtilley255@gmail.com>
2017-09-25 10:33:51 -0500

    P114 - couldn't resist / had an idea / experimenting & tinkering

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function car(color, year, make, miles)
 {
+	this.color = prompt("Enter vehicle color:"); 
+	this.year = year;
+	this.make = make;
+	this.miles = miles;
+	
+	console.log(this.color);
+}
+
+/*function car(color, year, make, miles)
+{
 	this.color = color; 
 	this.year = year;
 	this.make = make;
 	this.miles = miles;
-}
+}*/
