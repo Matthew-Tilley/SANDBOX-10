a34697d2fc93957cd7c6768e38717e3e1f19f245
Matt Tilley <mtilley255@gmail.com>
2017-09-25 11:19:10 -0500

    P115 - example code 8.3 / trying to output to console, but not working.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-person.prototype.sayHello = function()
+function person(personName)
+{
+	this.name = personName;
+	this.info = "This person is called " + this.name;
+	
+	this.showInfo = function()
+	{
+		alert(this.name + " says hello!");
+	}
+}
+
+
+/*person.prototype.sayHello = function()
 {
 	alert(this.name + " says hello!");
-}
+}*/
+
+person("Joe");
