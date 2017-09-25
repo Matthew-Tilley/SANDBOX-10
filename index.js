7ef9ac22bd1206973eefbc6b2b3a4da28f6610a9
Matt Tilley <mtilley255@gmail.com>
2017-09-25 10:43:19 -0500

    P114 - more work on same code; probably know what I did wrong...

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ function car(color, year, make, miles)
 	
 }
 
-/*function car(color, year, make, miles)
-{
-	this.color = color; 
-	this.year = year;
-	this.make = make;
-	this.miles = miles;
-}*/
+var car1 = new car(white, 2008, Pontiac, 110000);
+
+console.log(car1);
