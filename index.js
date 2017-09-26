362e237dce11246dd62a9e0c5267e535864fe677
Matt Tilley <mtilley255@gmail.com>
2017-09-26 09:29:35 -0500

    Adding numbers to empty array...off to a good start.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 function picker()
 {
-	var white_balls = "";
+	var white_balls = [];
 	
 	var possible = prompt("Enter a number:")
 	
-	white_balls += possible;
+	white_balls.push(possible);
 	
 	console.log(white_balls);
 	
